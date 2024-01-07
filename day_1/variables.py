2bc0e5e71fbb452b1e8eefccd45751b05b2668bf
Lizaveta Karpovich <maksimovna.elizaveta@gmail.com>
2024-01-07 23:17:38 +0100

    Learn variables and variables naming

---
+++ day_1/variables.py
@@ -0,0 +1,13 @@
+# Write a program that switches the values stored in the variables a and b, you should not have to type any numbers in your code.
+c = input('a = ')
+d = input('b = ')
+print('b = ' + c)
+print('a = ' + d)
+# OR
+a = input('a = ')
+b = input('b = ')
+c = a
+a = b
+b = c
+print('a = ' + a)
+print('b = ' + b)
