e072a28701333d931465c759d1605f0c26f0022c
Lizaveta Karpovich <maksimovna.elizaveta@gmail.com>
2024-01-07 22:16:25 +0100

    Learning print function

---
+++ day_1/print_function.py
@@ -0,0 +1,9 @@
+
+print("Day 1 - Python Print Functon")
+print("The function is declared like this:")
+print('print("What to print")')
+
+print('Hello World!\nHello World!')
+print('Hello World\nMy name is Liza')
+
+print('Hello'+' Liza')
