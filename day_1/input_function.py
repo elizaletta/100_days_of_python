d74817e04b95d1788c19251490d13ed95f366e36
Lizaveta Karpovich <maksimovna.elizaveta@gmail.com>
2024-01-07 22:52:49 +0100

    Learnt input fucntion

---
+++ day_1/input_function.py
@@ -0,0 +1,7 @@
+# print Hello Liza!
+# the code will be the following: print("Hello " + input("What is your name?") + "!")
+# Exercise: Write a program that prints the number of characters in a user's name
+s = input('What is your name? ')
+print(len(s))
+#OR another way
+print(len(input('What is your name? ')))
