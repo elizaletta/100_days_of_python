15217c4fdad8b3cc5e93f196750c18e9dd0ab461
Lizaveta Karpovich <maksimovna.elizaveta@gmail.com>
2024-01-08 17:08:43 +0100

    Learned about floor division, += operator and f-string

---
+++ day_2/number_manipulation.py
@@ -0,0 +1,9 @@
+number = 8 // 3
+number += 4
+print(number)
+
+score = 0
+height = 1.8
+isWinning = True
+#f-String
+print(f"Your score is {score}, your height is {height} and you are winning is {isWinning}")
