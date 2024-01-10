28cd5a4aaae104e5b1d325633ff0482eaecf6bf4
Lizaveta Karpovich <maksimovna.elizaveta@gmail.com>
2024-01-10 23:26:27 +0100

    adjusted the file

--- day_3/treasure_island.py
+++ day_3/treasure_island.py
@@ -22,11 +22,11 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
-crossroads_choice = (input("You're at a crossroad. Where do you want to go? Type \"left\" or \"right\"")).lower()
+crossroads_choice = (input("You're at a crossroad. Where do you want to go? Type \"left\" or \"right\" ")).lower()
 if crossroads_choice == "left":
-    water_choice = (input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across.")).lower()
+    water_choice = (input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across. ")).lower()
     if water_choice == "wait":
-        door_choice = (input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")).lower()
+        door_choice = (input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ")).lower()
         if door_choice == "red":
             print("It's a room full of fire. Game Over.")
         elif door_choice == "blue":
