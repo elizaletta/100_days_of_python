baa222d52558234bdf7b146c2929e2aaa681ad5e
Lizaveta Karpovich <maksimovna.elizaveta@gmail.com>
2024-01-11 20:26:06 +0100

    Created a program hat randomly chooses who pays the bill

---
+++ day_4/whos_paying_exercise.py
@@ -0,0 +1,11 @@
+#You are going to write a program which will select a random name from a list of names.
+#The person selected will have to pay for everybody's food bill.
+names_string = input("Give me everybody's names, separated by a comma. ")
+names_separated = names_string.split(", ")
+print(names_separated)
+number_of_names = len(names_separated)
+import random
+random_number = random.randint(0, number_of_names - 1)
+whos_paying = names_separated[random_number]
+print(whos_paying + " is going to pay the bill today!")
+
