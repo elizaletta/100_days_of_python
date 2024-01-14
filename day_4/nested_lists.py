1036d8f98ab69176d6b7b663b6ac1add3ea6e77c
Lizaveta Karpovich <maksimovna.elizaveta@gmail.com>
2024-01-14 17:26:55 +0100

    Exercised nested lists by creating a treasure map

--- day_4/nested_lists.py
+++ day_4/nested_lists.py
@@ -2,4 +2,6 @@ dirty_dozen = ['strawberry', 'spinach', 'kale', 'nectarines', 'apples', 'grapes'
 fruits = ['strawberry', 'nectarines', 'apples', 'grapes', 'peaches', 'cherries', 'pears']
 vegetables = ['spinach', 'kale', 'tomatoes', 'celery', 'potatoes']
 dirty_dozen = [fruits, vegetables]
-print(dirty_dozen)
+print(dirty_dozen[0])
+print(dirty_dozen[1])
+print(dirty_dozen[0][0])
