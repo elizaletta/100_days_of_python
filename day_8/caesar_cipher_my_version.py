96f01f94809ba58588b9c29b7ff8048fef50203d
Lizaveta Karpovich <maksimovna.elizaveta@gmail.com>
2024-02-23 13:10:33 +0100

    Minor changes

--- day_8/caesar_cipher_my_version.py
+++ day_8/caesar_cipher_my_version.py
@@ -1,31 +1,21 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def caesar(input_text, shift_amount, cipher_direction):
+    output_text = ""
+    for letter in input_text:
+        position = alphabet.index(letter)
+        if cipher_direction == "encode":
+            new_position = position + shift_amount
+            new_letter = alphabet[new_position]
+            output_text += new_letter
+        elif cipher_direction == "decode":
+            new_position = position - shift_amount
+            new_letter = alphabet[new_position]
+            output_text += new_letter
+    print(f"The text is {output_text}")
 
-def encrypt(plain_text, shift_amount):
-    encrypted_text = []
-    for i in range(len(plain_text)):
-        indx = alphabet.index(text[i])
-        new_indx = indx + shift_amount
-        encrypted_text.append(alphabet[new_indx])
-    print(''.join(encrypted_text))
-
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-    #TODO2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+caesar(input_text=text, shift_amount=shift, cipher_direction=direction)
